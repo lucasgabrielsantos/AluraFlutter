138291ebc390d32850fac6a9d291092914dabe47
lucasgabrielsantos <lucas.gsantos@live.com>
2020-07-21 22:52:24 -0300

    criando a tela de formulario e a model contact para a transicao de dados

--- bytebankpersistencia/lib/screens/contacts_list.dart
+++ bytebankpersistencia/lib/screens/contacts_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebankpersistencia/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
@@ -24,7 +25,15 @@ class ContactsList extends StatelessWidget {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+                MaterialPageRoute(
+                  builder: (context) => ContactForm(),
+                ),
+              )
+              .then((newContact) => debugPrint(newContact.toString()));
+        },
         child: Icon(Icons.add),
       ),
     );
